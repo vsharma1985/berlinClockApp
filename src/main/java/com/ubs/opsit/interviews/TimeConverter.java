04a1feb7aba4ffc81846ccd4b650051ecfdfa282
vivek sharma <vsharma1985@gmail.com>
2018-05-06 01:54:46 +0530

    Changes Done for First Time

--- src/main/java/com/ubs/opsit/interviews/TimeConverter.java
+++ src/main/java/com/ubs/opsit/interviews/TimeConverter.java
@@ -3,6 +3,8 @@ package com.ubs.opsit.interviews;
 public interface TimeConverter {
 
     String convertTime(String aTime);
-	
+	String getLamps(int timeUnitValue);
+	String getLamps(int timeUnitValue, int numericValue,String strLamp);
+	String getLamps(int timeUnitValue, int numericValue,String strRedLamp, String strYellowLamp);
 
 }
